0ac6f5d9c94e9cfe5fc6e44a7e63e7b2b5273f0e
Randy177 <154349939+Randy177@users.noreply.github.com>
2023-12-19 23:31:54 +0800

    BSIT
    BSIT 1G Malingin

---
+++ MALINGIN WEBPAGE/register.js
@@ -0,0 +1,12 @@
+function getUsername() {
+    var username = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || email === "" || password === "") {
+        document.getElementById("result").innerHTML = "Please fill in all fields.";
+    } else {
+        document.getElementById("result").innerHTML = "Registration successful! Welcome, " + username + "!";
+        window.location.href = 'login.html';  
+    }
+}
